176c6f6d9877ae44d4e216cab7f8c9f10bceae67
harshadalohar123 <harshalohar123@gmail.com>
2022-04-03 21:16:53 +0530

    ADDED:User First Name

---
+++ UserRegistrationException/UserRegistrationException/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UserRegistrationException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the regex Program ");
+
+            string FirstName;
+            Console.Write("Enter your First Name : ");
+            FirstName = Console.ReadLine();
+            RegexProblem.checkFirstName(FirstName);
+
+        }
+    }
+}
